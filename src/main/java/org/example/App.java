4a94949f038d0ca74e6d0cf32742d0fbc7484e5d
bCommit2024 <brigittehounkpe@cmail.carleton.ca>
2024-02-28 18:56:36 -0500

    Version 1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.math.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String returnFizzBuzz(int num){
+        if (Math.ceilMod(num,15)==0){
+            return "FizzBuzz";
+        }else if (Math.ceilMod(num,5)==0){
+            return "Buzz";
+        }else if (Math.ceilMod(num,3)==0){
+            return "Fizz";
+        }else{
+            return Integer.toString(num);
+        }
+    }
+}
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static org.example.App.returnFizzBuzz;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+
+
+
+    public void testApp()
+    {assertTrue( true );}
+
+    public void testFizzBuzz(){
+        String strFizzBuzz;
+        for (int i = 0; i<=100; i++){
+            strFizzBuzz = returnFizzBuzz(i);
+
+            if (Math.ceilMod(i,15) == 0) {
+                assertEquals("FizzBuzz", strFizzBuzz);
+            } else if (Math.ceilMod(i,5) == 0) {
+                assertEquals("Buzz", strFizzBuzz);
+            }else if (Math.ceilMod(i,3) == 0) {
+                assertEquals("Fizz", strFizzBuzz);
+            } else {
+                assertEquals(Integer.toString(i), strFizzBuzz);
+            }
+        }
+    }
+}
